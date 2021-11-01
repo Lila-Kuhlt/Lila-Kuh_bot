0b8fa18023c635632d3a9dc8975c8ba511689a09
EliasSchaut <eschaut@outlook.de>
2021-11-02 00:28:25 +0100

    update package & bug fixes

--- js/slash_commands/option_models/help.js
+++ js/slash_commands/option_models/help.js
@@ -1,13 +1,11 @@
-module.exports = [
+lmodule.exports = [
     {
         "name": "command",
         "description": "information for the specific command",
         "required": false,
         "choices": [
             { "name": "help", "value": "help" },
-            { "name": "reload", "value": "reload" },
             { "name": "lang", "value": "lang" },
-            { "name": "prefix", "value": "prefix" },
             { "name": "echo", "value": "echo" },
             { "name": "ping", "value": "ping" },
         ]
