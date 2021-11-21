9975621795b9e8aa3ef9e5bdfd43b02c00e3610d
EliasSchaut <eschaut@outlook.de>
2021-11-21 17:52:37 +0100

    add scheduler and mensa_manager

--- js/event_helper/events.js
+++ js/event_helper/events.js
@@ -1,11 +1,12 @@
 // helper fields
 const s = "events."
+const schedule = require('node-schedule')
 
 // ---------------------------------
 // Export
 // ---------------------------------
 async function init(client) {
-
+    init_mensa_event(client)
 }
 
 async function event_create(msg) {
@@ -15,9 +16,20 @@ async function event_create(msg) {
 
 
 // ----------------------------------
-// Events
+// Inits
+// ----------------------------------
+function init_mensa_event(client) {
+    schedule.scheduleJob('0 0 6 * * 1-5', mensa_event(client)) // Mo-Fr at 6:00 am
+}
 // ----------------------------------
 
+
+// ----------------------------------
+// Events
+// ----------------------------------
+function mensa_event(client) {
+    return require("../mensa_manager").post_mensa_plan(client)
+}
 // ----------------------------------
 
 
---
+++ js/mensa_manager.js
@@ -0,0 +1,18 @@
+const s = ""
+
+const mensa_link = "https://www.sw-ka.de/de/essen/?kw=47"
+const mensa_channel_id = "695664840177352784"
+const used_mensa_lines = [
+    "Linie 1Gut & Günstig",
+    "Linie 2vegane Linie",
+    "Linie 3",
+    "Linie 4",
+    "Linie 5 To-Go",
+    "Linie 6"
+]
+
+function post_mensa_plan(client) {
+
+}
+
+module.exports = { post_mensa_plan }
