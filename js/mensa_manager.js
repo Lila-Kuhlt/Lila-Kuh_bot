efe47084e24c380879dd9321ec998ef5f982c765
EliasSchaut <eschaut@outlook.de>
2021-12-20 18:15:27 +0100

    add text format and send

--- js/mensa_manager.js
+++ js/mensa_manager.js
@@ -2,27 +2,25 @@ const s = ""
 const dayjs = require('dayjs')
 const axios = require('axios')
 const { logger } = require("./logger")
+const { MessageEmbed } = require('discord.js');
 
 const meal_api = {
     start: "https://openmensa.org/api/v2/canteens/31/days/",
     end: "/meals"
 }
 const mensa_channel_id = "695664840177352784"
-const used_mensa_lines = [
-    "Linie 1Gut & Günstig",
-    "Linie 2vegane Linie",
-    "Linie 3",
-    "Linie 4",
-    "Linie 5 To-Go",
-    "Linie 6"
-]
 
 // ---------------------------------
 // Export
 // ---------------------------------
 async function post_mensa_plan(client) {
-    const response = await get_meal_json(get_meal_api_link())
-    console.log(response)
+    const data = await get_meal_json(get_meal_api_link())
+
+    if (data === null) send_fail(client)
+    else {
+        const fields = formatted_meals_to_fields(format_meals(data))
+        send_success(client, fields)
+    }
 }
 // ---------------------------------
 
@@ -31,16 +29,77 @@ async function post_mensa_plan(client) {
 // ---------------------------------
 // Send
 // ---------------------------------
+async function send_fail(client) {
+    const channel = client.channels.cache.get(mensa_channel_id)
+    const embed = new MessageEmbed()
+        .setColor(client.config.embed.color)
+        .setTitle("Mensa Update Error")
+        .setDescription("[Test 0]")
 
+    channel.send({ embeds: [embed] })
+}
+
+async function send_success(client, fields) {
+    const channel = client.channels.cache.get(mensa_channel_id)
+    const embed = new MessageEmbed()
+        .setColor(client.config.embed.color)
+        .setTitle("Mensa Update für " + get_date())
+        .addFields(fields)
+        .setURL("https://www.sw-ka.de/de/essen/?kw=51#fragment-c1-2")
+
+    channel.send({ embeds: [embed] })
+}
 // ---------------------------------
 
+
+// ---------------------------------
+// Format
+// ---------------------------------
+function format_meals(data) {
+    const output = {}
+
+    for (const meal of data) {
+        if (Object.keys(output).includes(meal.category)) {
+            output[meal.category].push(get_meals_name_and_price(meal))
+
+        } else {
+            output[meal.category] = [ get_meals_name_and_price(meal) ]
+        }
+    }
+    return output
+}
+
+function get_meals_name_and_price(meal) {
+    return { name: meal.name, price: meal.prices.students }
+}
+
+function formatted_meals_to_fields(meals) {
+    const fields = []
+    for (const category of Object.keys(meals)) {
+        let field_values = []
+        for (const value of meals[category]) {
+            const price = (value.price === null) ? 0.0 : value.price
+            field_values.push(`${value.name} (${price}€)`)
+        }
+        if (field_values.length === 0) continue
+
+        fields.push({ name: category, value: field_values.join("\n") + "\n" })
+    }
+
+    return fields
+}
+// ---------------------------------
 
 
 // ---------------------------------
 // API
 // ---------------------------------
+function get_date() {
+    return dayjs().add(1, 'day').format('YYYY-MM-DD')
+}
+
 function get_meal_api_link() {
-    return meal_api.start + dayjs().add(1, 'day').format('YYYY-MM-DD') + meal_api.end
+    return meal_api.start + get_date() + meal_api.end
 }
 
 async function get_meal_json(api_link) {
