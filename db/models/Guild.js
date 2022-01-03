25a0528e525b09ed2d9517e7adaed67648b52106
EliasSchaut <eschaut@outlook.de>
2022-01-03 15:29:43 +0100

    add mensa functionalities to GUILD DB

--- db/models/Guild.js
+++ db/models/Guild.js
@@ -8,6 +8,11 @@ const _TABLE = (sequelize, Sequelize) => {
             unique: true,
         },
         prefix: Sequelize.TEXT,
+        mensa_channel_id: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+        mensa_enabled: Sequelize.BOOLEAN
     }, {
         timestamp: false
     })
@@ -25,7 +30,9 @@ async function add(msg) {
     try {
         await msg.client.DB.Guild.TABLE.create({
             guild_id: msg.member.guild.id,
-            prefix: msg.client.config.prefix
+            prefix: msg.client.config.prefix,
+            mensa_channel_id: null,
+            mensa_enabled: false
         })
         msg.client.logger.log("info",`guild ${msg.member.guild.name} successfully added to database 'Guild'`)
 
@@ -71,10 +78,66 @@ async function set_prefix(msg, new_prefix) {
     }
 }
 
+// get mensa_channel_id of the guild. If guild doesn't exist in database, the guild will add it into the database
+async function mensa_get_channel_id(msg) {
+    const tag = await msg.client.DB.Guild.TABLE.findOne({ where: { guild_id: msg.member.guild.id } })
+
+    if (tag) {
+        return tag.mensa_channel_id
+
+    } else {
+        msg.client.logger.log("warn",`guild ${msg.member.guild.name} not in database 'Guild'`)
+        await add(msg)
+        return await mensa_get_channel_id(msg)
+    }
+}
+
+// set mensa_channel_id
+async function mensa_set_channel_id(msg, new_channel_id) {
+    const old_channel_id = await mensa_get_channel_id(msg)
+    const new_tag = await msg.client.DB.Guild.TABLE.update({ mensa_channel_id: new_channel_id }, { where: { guild_id: msg.member.guild.id } })
+
+    if (new_tag) {
+        return true
+
+    } else {
+        msg.client.logger.log("error", `Could not set mensa_channel_id from ${old_channel_id} to ${new_channel_id} of guild ${msg.member.guild.name} in database 'Guild'`)
+        return false
+    }
+}
+
+// set prefix of the author from message
+async function mensa_enable(msg) {
+    const new_tag = await msg.client.DB.Guild.TABLE.update({ mensa_enable: true }, { where: { guild_id: msg.member.guild.id } })
+
+    if (new_tag) {
+        return true
+
+    } else {
+        msg.client.logger.log("warn",`guild ${msg.member.guild.name} not in database 'Guild'`)
+        await add(msg)
+        return await mensa_enable(msg)
+    }
+}
+
+// set prefix of the author from message
+async function mensa_disable(msg) {
+    const new_tag = await msg.client.DB.Guild.TABLE.update({ mensa_enable: false }, { where: { guild_id: msg.member.guild.id } })
+
+    if (new_tag) {
+        return true
+
+    } else {
+        msg.client.logger.log("warn",`guild ${msg.member.guild.name} not in database 'Guild'`)
+        await add(msg)
+        return await mensa_enable(msg)
+    }
+}
+
 async function remove(msg, guild_id) {
 
 }
 // ---------------------------------------------
 
 
-module.exports = { _TABLE, add, get_prefix, set_prefix, remove }
+module.exports = { _TABLE, add, get_prefix, set_prefix, mensa_get_channel_id, mensa_set_channel_id, mensa_enable, mensa_disable }
