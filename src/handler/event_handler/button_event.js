3a09c67e783b4706d73a699fc02882be805b0409
EliasSchaut <eschaut@outlook.de>
2022-01-05 18:54:55 +0100

    add button-event handler for poll

--- src/handler/event_handler/button_event.js
+++ src/handler/event_handler/button_event.js
@@ -3,7 +3,10 @@
 // Export
 // ---------------------------------
 async function interaction_create(interaction) {
+    if (interaction.customId.startsWith("poll-")) await button_private_poll(interaction)
     // add more specific button handlers here and below in Specific Button Handlers
+
+
 }
 // ---------------------------------
 
@@ -12,7 +15,12 @@ async function interaction_create(interaction) {
 // ---------------------------------
 // Specific Button Handlers
 // ---------------------------------
-
+async function button_private_poll(interaction) {
+    const poll_id = interaction.message.id
+    const vote = interaction.customId.split("-")[1]
+    const poll_vote_cmd = interaction.client.commands.get("poll_vote")
+    await poll_vote_cmd.execute(interaction.client.slash_event.interaction_to_message(interaction), [poll_id, vote])
+}
 // ---------------------------------
 
 
