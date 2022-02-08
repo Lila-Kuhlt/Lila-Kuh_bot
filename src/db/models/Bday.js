220dbbba8ff54f575730b828ed1bdab0cb9141a9
EliasSchaut <eschaut@outlook.de>
2022-02-08 19:23:53 +0100

    add guild_id in Bday table

--- src/db/models/Bday.js
+++ src/db/models/Bday.js
@@ -5,14 +5,20 @@
 // ---------------------------------------------
 // Model
 // ---------------------------------------------
-//TODO: Add guild_id
 const _TABLE = (sequelize, Sequelize) => {
     return sequelize.define('Bday', {
+        "guild_id": {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
         "user_id": {
             type: Sequelize.STRING,
-            unique: true,
+            allowNull: false
         },
-        "bdate": Sequelize.STRING, // YYYY-MM-DD
+        "bdate": { // YYYY-MM-DD
+            type: Sequelize.STRING,
+            allowNull: false
+        },
     }, {
         timestamp: false
     })
@@ -24,59 +30,60 @@ const _TABLE = (sequelize, Sequelize) => {
 // Helper
 // ---------------------------------------------
 // add stuff to database
-async function add(client, user_id, bdate) {
+async function add(client, guild_id, user_id, bdate) {
     try {
         await client.DB["Bday"].TABLE.create({
+            "guild_id": guild_id,
             "user_id": user_id,
             "bdate": bdate
         })
 
     } catch (e) {
         if (e.name === 'SequelizeUniqueConstraintError') {
-            client.logger.log("warn",`${user_id} already exist in database Bday!`)
+            client.logger.log("warn",`${guild_id} x ${user_id} already exist in database Bday!`)
 
         } else {
-            client.logger.log("error",`Something went wrong with adding ${user_id} in database Bday!`)
+            client.logger.log("error",`Something went wrong with adding ${guild_id} x ${user_id} in database Bday!`)
         }
     }
 }
 
 // get stuff from database
-async function get(client, user_id) {
+async function get(client, guild_id, user_id) {
     const tag = await client.DB["Bday"].TABLE.findOne({ where: { "user_id": user_id } })
 
     if (tag) {
         return tag.lang
 
     } else {
-        client.logger.log("warn",`${user_id} not in database Bday!`)
+        client.logger.log("warn",`${guild_id} x ${user_id} not in database Bday!`)
         return null
     }
 }
 
 
 // set stuff in database
-async function set(client, user_id, bdate) {
-    const new_tag = await client.DB["Bday"].TABLE.update({ "bdate": bdate }, { where: { "user_id": user_id } })
+async function set(client, guild_id, user_id, bdate) {
+    const new_tag = await client.DB["Bday"].TABLE.update({ "bdate": bdate }, { where: { "guild_id": guild_id, "user_id": user_id } })
 
     if (new_tag) {
         return true
 
     } else {
-        client.logger.log("error", `Could not set <attribute2>!`)
+        client.logger.log("error", `Could not set ${guild_id} x ${user_id} in database Bday!`)
         return false
     }
 }
 
 // remove tag in database
-async function remove(client, user_id) {
-    const rowCount = await client.DB["Bday"].TABLE.destroy({ where: { "user_id": user_id } })
+async function remove(client, guild_id, user_id) {
+    const rowCount = await client.DB["Bday"].TABLE.destroy({ where: { "guild_id": guild_id, "user_id": user_id } })
 
     if (rowCount) {
         return true
 
     } else {
-        client.logger.log("error", `Could not delete tag with ${user_id}!`)
+        client.logger.log("error", `Could not delete tag with ${guild_id} x ${user_id}!`)
         return false
     }
 }
