8bae24300a4d7f17a830dba4c5c6df01a4ce5463
EliasSchaut <eschaut@outlook.de>
2022-02-08 04:56:32 +0100

    fill skeleton

--- src/commands/bday/bday_all.js
+++ src/commands/bday/bday_all.js
@@ -1,24 +1,20 @@
 // See also: https://github.com/EliasSchaut/Discord-Bot-Template/wiki/How-to-command
 
 const { get_text: gt } = require("../../lang/lang_man")
-const s = "commands.<name>."
+const s = "commands.bday_all."
 
 module.exports = {
-    name: '<name>',
+    name: 'bday_all',
     description: async function (msg) { return await gt(msg, s + "help") },
-    aliases: ['<alias_1>', '<alias_2>'],
+    aliases: ['bdaya', 'bda'],
     args_needed: false,
     args_min_length: 0,
     args_max_length: 0,
-    usage: async function (msg) { return await gt(msg, s + "usage") },
-    guild_only: false,
-    dm_only: false,
-    need_permission: ['<permission1>', '<permission2>'],
-    admin_only: false,
-    nsfw: false,
+    guild_only: true,
+    need_permission: [],
     disabled: false,
     enable_slash: false,
-    async execute(msg, args) {  // msg = discord.js 'Message' object; args = given arguments as list
-        // your lovely code to execute
+    async execute(msg, args) {
+
     },
 };
