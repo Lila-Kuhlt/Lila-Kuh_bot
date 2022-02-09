38141298f69ff54de0193a8529ac5861fcaaab98
EliasSchaut <eschaut@outlook.de>
2022-02-09 02:19:00 +0100

    implement bday opt in command

--- src/commands/bday/bday_opt_in.js
+++ src/commands/bday/bday_opt_in.js
@@ -1,6 +1,7 @@
 // See also: https://github.com/EliasSchaut/Discord-Bot-Template/wiki/How-to-command
 
 const { get_text: gt } = require("../../lang/lang_man")
+const dayjs = require("dayjs");
 const s = "commands.bday_opt_in."
 
 module.exports = {
@@ -15,7 +16,18 @@ module.exports = {
     need_permission: [],
     disabled: false,
     enable_slash: false,
-    async execute(msg, args) {
+    async execute(msg, args) { // args: YYYY-MM-DD
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(args[0])) return await msg.client.output.reply(msg, await gt(msg, `${s}fail.wrong_format`))
 
+        const date = dayjs(args[0], "YYYY-MM-DD")
+        if (!date.isValid) return await msg.client.output.reply(msg, await gt(msg, `${s}fail.wrong_format`))
+        if (dayjs().isBefore(date)) return await msg.client.output.reply(msg, await gt(msg, `${s}fail.in_future`))
+
+
+        if (!await msg.client.DB.Bday.set(msg.client, msg.guildId, msg.author.id, date.year(), date.month(), date.date())) {
+            await msg.client.DB.Bday.add(msg.client, msg.guildId, msg.author.id, date.year(), date.month(), date.date())
+        }
+
+        await msg.client.output.send(msg, await gt(msg, `${s}success`))
     },
 };
