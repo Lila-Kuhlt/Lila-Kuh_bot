cdbc38bdfc96bda05f058c93e86c970ce1c92410
EliasSchaut <eschaut@outlook.de>
2022-02-09 04:53:48 +0100

    implement command bday_all

--- src/commands/bday/bday_all.js
+++ src/commands/bday/bday_all.js
@@ -1,7 +1,9 @@
 // See also: https://github.com/EliasSchaut/Discord-Bot-Template/wiki/How-to-command
 
 const { get_text: gt } = require("../../lang/lang_man")
+const dayjs = require("dayjs");
 const s = "commands.bday_all."
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: 'bday_all',
@@ -15,6 +17,38 @@ module.exports = {
     disabled: false,
     enable_slash: false,
     async execute(msg, args) {
+        const user_ids = await msg.client.DB.Bday.get_user_ids(msg.client, msg.guildId)
+        const months = new Array(12).fill(0).map(() => { return [] })
 
+        for (const user_id of user_ids) {
+            try {
+                await msg.guild.members.fetch(user_id)
+            } catch (e) {
+                continue
+            }
+            const tag = await msg.client.DB.Bday.get(msg.client, msg.guildId, user_id)
+            if (tag === null) continue
+
+            const bdate = dayjs(new Date(tag.year, tag.month, tag.day)).format(msg.client.config.date.format)
+            console.log(months)
+            months[tag.month].push(await gt(msg, `${s}embed.bday_entry`, bdate, user_id))
+            console.log(months)
+        }
+
+        const embed = new MessageEmbed()
+            .setAuthor({ name: msg.client.config.embed.author_name, iconURL: msg.client.config.embed.avatar_url })
+            .setColor(msg.client.config.embed.color)
+            .setTitle(await gt(msg, `${s}embed.title`))
+
+
+        for (let i = 0; i < months.length; i++) {
+            if (months[i].length === 0) continue
+
+            const month_name = await gt(msg, `${s}months.${i}`)
+            months[i].sort()
+            embed.addField(month_name, months[i].join("\n"), true)
+        }
+
+        msg.client.output.send(msg, { embeds: [embed] })
     },
 };
