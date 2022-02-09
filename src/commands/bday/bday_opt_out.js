fcf227153839a2045626d13ace551a3c57860073
EliasSchaut <eschaut@outlook.de>
2022-02-09 02:28:54 +0100

    implement bday command opt out

--- src/commands/bday/bday_opt_out.js
+++ src/commands/bday/bday_opt_out.js
@@ -6,7 +6,7 @@ const s = "commands.bday_opt_out."
 module.exports = {
     name: 'bday_opt_out',
     description: async function (msg) { return await gt(msg, s + "help") },
-    aliases: ['bdayou', 'bdou'],
+    aliases: ['bdayoo', 'bdoo'],
     args_needed: false,
     args_min_length: 0,
     args_max_length: 0,
@@ -15,6 +15,11 @@ module.exports = {
     disabled: false,
     enable_slash: false,
     async execute(msg, args) {
+        if (await msg.client.DB.Bday.remove(msg.client, msg.guildId, msg.author.id)) {
+            await msg.client.output.reply(msg, await gt(msg, `${s}success`))
 
+        } else {
+            await msg.client.output.reply(msg, await gt(msg, `${s}fail.not_opt_in`))
+        }
     },
 };
