28c079f9bf50bb235a12aae2c5dde7f87446cefc
EliasSchaut <eschaut@outlook.de>
2022-02-15 22:35:38 +0100

    implement accordion generation

--- docs/res/js/main.js
+++ docs/res/js/main.js
@@ -1,10 +1,13 @@
 import { get_config, get_lang_jsons } from './file_getter.js'
+import { generate_accordion_elements } from './gen_cmd_accordion.js'
 
-main()
+document.body.onload = main()
 
 async function main() {
     const config = await get_config()
     const langs = await get_lang_jsons(config)
+
+    generate_accordion_elements(langs.de)
     console.log(config)
     console.log(langs)
 }
