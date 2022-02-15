7dd7c87e199e77fc0cb544b7e51c8b654220c5a3
EliasSchaut <eschaut@outlook.de>
2022-02-15 22:41:08 +0100

    remove comments

--- docs/res/js/gen_cmd_accordion.js
+++ docs/res/js/gen_cmd_accordion.js
@@ -1,20 +1,3 @@
-/**
- * <div class="accordion" id="accordionExample">
- *     <div class="accordion-item">
- *         <h2 class="accordion-header" id="headingOne">
- *             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
- *                 <b>help</b>
- *             </button>
- *         </h2>
- *         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
- *             <div class="accordion-body">
- *                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
- *             </div>
- *         </div>
- *     </div>
- * </div>
- *
- */
 
 const accordion_base_id = "commands"
 const accordion_element = document.getElementById(accordion_base_id)
